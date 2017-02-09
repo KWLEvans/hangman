d8093778e499c537e03344ab70e3b45a67b4de32
Jennifer Beem and Keith Evans <jennifer@>
2017-02-09 14:53:35 -0800

    add progress tracking toward guessing answer

--- app/app.php
+++ app/app.php
@@ -4,6 +4,11 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/hangman.php";
 
+    session_start();
+    if (empty($_SESSION['game'])) {
+        $_SESSION['game'] = "";
+    }
+
     $app = new Silex\Application();
 
     $app["debug"] = true;
@@ -11,8 +16,29 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), ['twig.path' => __DIR__.'/../views']);
 
     $app->get("/", function() use ($app) {
+        $new_game = new Game("butts");
+        $new_game->save();
+        return $app["twig"]->render("new_game.html.twig");
+    });
+
+    $app->get("/guess", function() use ($app) {
         return $app["twig"]->render("home.html.twig");
     });
+
+    $app->post("/hangman", function() use ($app) {
+        $user_guess = strtolower($_POST['guess']);
+        $game = Game::getGame();
+        $result = $game->check($user_guess);
+        $game->save();
+        if ($result) {
+            $result = $game->spaceify();
+        } else {
+            $result = "Wrong.";
+        }
+        return $app["twig"]->render("hangman.html.twig", ["result" => $result]);
+    });
+
+
 
     return $app;
 ?>
--- src/hangman.php
+++ src/hangman.php
@@ -3,13 +3,58 @@ class Game
 {
     private $letters_guessed;
     private $answer;
+    private $current_string;
     private $guesses;
 
     function __construct($answer)
     {
         $this->letters_guessed = [];
-        $this->answer = $answer;
+        $this->answer = strtolower($answer);
+        $this->current_string;
+        for ($i = 0; $i < strlen($answer); $i++) {
+            $this->current_string.="_";
+        }
         $this->guesses = 5;
+    }
+
+    function check($user_guess)
+    {
+        array_push($this->letters_guessed, $user_guess);
+        $answers_array = [];
+        for ($i = 0; $i < strlen($this->answer); $i++) {
+            if ($user_guess == $this->answer[$i]) {
+                array_push($answers_array, $i);
+            }
+        }
+
+        if ($answers_array) {
+            foreach ($answers_array as $index) {
+                $this->current_string[$index] = $user_guess;
+            }
+            return $this->current_string;
+        } else {
+            $this->guesses--;
+            return false;
+        }
+    }
+
+    function spaceify()
+    {
+        $spacified_string = "";
+        for ($i = 0; $i < strlen($this->current_string); $i++) {
+            $spacified_string.= $this->current_string[$i] . " ";
+        }
+        return $spacified_string;
+    }
+
+    function save()
+    {
+        $_SESSION["game"] = $this;
+    }
+
+    static function getGame()
+    {
+        return $_SESSION["game"];
     }
 
 }
